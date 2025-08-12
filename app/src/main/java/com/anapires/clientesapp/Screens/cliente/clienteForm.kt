e26c35fb307a8c037d84b410f78830070ab99acc
ANA CLARA RIMUALDO PIRES <24122542@dsn127m1242374.cfp127aluno.local>
2025-08-12 14:40:49 -0300

    Atualizado

--- app/src/main/java/com/anapires/clientesapp/Screens/cliente/clienteForm.kt
+++ app/src/main/java/com/anapires/clientesapp/Screens/cliente/clienteForm.kt
@@ -1,9 +1,53 @@
 package com.anapires.clientesapp.Screens.cliente
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun CienteForm(){
+fun ClienteForm() {
+    Column(
+        modifier = Modifier
+            .background(
+                MaterialTheme
+                    .colorScheme
+                    .primaryContainer
+            )
+            .fillMaxSize(),
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                imageVector = Icons.Default.Person,
+                contentDescription = "Person"
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            Text(
+                text = "Novo Perfil",
+                style = MaterialTheme.typography.titleLarge
+            )
+        }
+    }
+}
 
+@Preview(showBackground = true)
+@Composable
+private fun ClienteFormPreview() {
+    ClienteForm()
 }
